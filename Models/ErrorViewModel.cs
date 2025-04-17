e0784ab385af8f7ed2a50fbb6617042a808f7f35
VuongThiHongPhuc <pv6025934@gmail.com>
2025-04-17 08:05:20 +0700

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bài_4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
